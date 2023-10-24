97206ed4038445f7c8f618bc4e03d36d6ece975c
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-10-24 10:16:59 +0200

    Add files via upload

---
+++ CA231024/CA231024/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA231024
+{
+    internal class Bolygo
+    {
+        public string BolygoNeve { get; set; }
+        public int HoldakSzama { get; set; }
+        public double Arany { get; set; }
+
+        public Bolygo(string sor)
+        {
+            var atmeneti = sor.Split(';');
+            this.BolygoNeve = atmeneti[0];
+            this.HoldakSzama = int.Parse(atmeneti[1]);
+            this.Arany = double.Parse(atmeneti[2]);
+        }
+    }
+}
---
+++ CA231024/CA231024/Program.cs
@@ -0,0 +1,58 @@
+﻿using CA231024;
+using System.Linq;
+using System.Text;
+
+var bolygok = new List<Bolygo>();
+
+var sr = new StreamReader(
+    path:@"..\..\..\src\solsys.txt",
+    encoding: Encoding.UTF8);
+
+while (!sr.EndOfStream)
+    bolygok.Add(new Bolygo(sr.ReadLine()));
+
+Console.WriteLine("3. feladat:");
+
+Console.WriteLine($"\t3.1: {bolygok.Count()} bolygó van a naprendszerben");
+
+var f4 = bolygok
+    .Average(b => b.HoldakSzama);
+
+Console.WriteLine($"\t3.2: a naprendszerben egy bolygónak átlagosan {f4:0.00} holdja van!");
+
+var f5 = bolygok
+    .Where(b => b.Arany == bolygok.Max(b => b.Arany))
+    .First();
+
+Console.WriteLine($"\t3.3: a legnagyobb térfogatú bolygó a {f5.BolygoNeve}");
+
+Console.Write("\t3.4 Írd be a keresett bolygó nevét: ");
+var keresett = Console.ReadLine();
+
+var f6 = bolygok
+    .Where(b => b.BolygoNeve.Contains(keresett));
+
+if (f6.Count() >= 1)
+{
+    Console.WriteLine("\t\tVan ilyen nevű bolygó a naprendszerben!");
+}
+else 
+{
+    Console.WriteLine("\t\tNincs ilyen nevű bolygó a naprednszerben!");
+}
+
+Console.Write("\t3.5 Írj be egy egész számot: ");
+int keresettSzam = int.Parse(Console.ReadLine());
+
+var f7 = bolygok
+    .Where(b => b.HoldakSzama > keresettSzam)
+    .ToList();
+
+Console.WriteLine($"\t\ta következő bolygóknak van {keresettSzam}-nál/nél több holdja:");
+Console.Write("\t\t[");
+foreach (var f in f7)
+    if(f == f7.Last())
+        Console.Write($"'{f.BolygoNeve}'");
+    else
+        Console.Write($"'{f.BolygoNeve}', ");
+Console.Write("]");
